dc204957c45a2e21a9a9120c6de9a7e0240aa046
github username <github email address>
2019-01-23 14:13:47 -0500

    mvp

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ const Home = (props) => {
 <div>
 
         <h5>Postionless Fantasy for a Postionless NBA</h5>
-        <h6>If you are a fan of the NBA you may have heard of the term postionless basketball. You no longer have to be held back by the constraints of traditional fantasy. In NBA Dream Match, postion doesn't matter! Build the best team possible and keep under budget. See if you can beat the computer. Click a player to add or remove him from your team. Once you have selected a press Generate Matchup to see your opponent. Press simulate to see if you can beat the computer. </h6>
+        <h6>If you are a fan of the NBA you may have heard of the term postionless basketball. You no longer have to be held back by the constraints of traditional fantasy. In NBA Dream Match, postion doesn't matter! Build the best team possible and keep under budget. See if you can beat the computer. Click a player to add or remove him from your team. Once you have selected 5 players click Generate Matchup to see your opponent. Press simulate to see if you can beat the computer. Good Luck! </h6>
         <h3>Select 5 Players. Stay Under the Budget</h3>
 
 
